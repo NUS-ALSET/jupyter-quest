60a071b6d481a91e7fa41b19e394ee21ab63706d
sarabjeet singh <developer.up90@gmail.com>
2018-02-20 15:04:27 +0530

    assignment visibility and solution visibility functionality

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Switch,Route} from 'react-router-dom'
 import App from './components/app'
 import Courses from './pages/coursePage'
-import {CourseDetail} from './components/courses/'
+import {AssignmentWithFirebase} from './components/courses/'
 import {LoginRoute, PrivateRoute} from './privateRoute';
 import PrivateInstructorRoute from './privateInstructorRoute';
 import {Paths} from './components/path/path';
@@ -12,7 +12,7 @@ import Page404 from './components/404'
 
 const Root=()=>(
   <Switch>
-    <PrivateInstructorRoute path="/courses/:id" component={CourseDetail} />
+    <PrivateInstructorRoute path="/courses/:id" component={AssignmentWithFirebase} />
     <PrivateRoute path="/courses" exact component={Courses} />
     <PrivateRoute path="/path" component={Paths} />
     <LoginRoute path="/login" component={Login} />
